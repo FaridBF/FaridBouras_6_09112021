b2b16758b94602e2dae2cfde1a668a02b6509163
Bouras Farid <bouras.farid@outlook.fr>
2021-11-20 17:31:50 +0100

    chore: Added  .env file

--- server.js
+++ server.js
@@ -3,5 +3,9 @@ const server = http.createServer((req, res) => {
     res.end('Voila la réponse du serveur') 
 });
 
+// chargement des variables d'environnement du fichier .env dans process.env
+const dotenv = require("dotenv");
+dotenv.config();
+
 // écouter le serveur sur le port choisi ou  port 3000 par defaut
 server.listen(process.env.PORT || 3000);

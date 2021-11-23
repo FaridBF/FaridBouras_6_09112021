a0fbbae1621fa1745523fe94a73ebbe17b975573
Bouras Farid <bouras.farid@outlook.fr>
2021-11-23 19:14:08 +0100

    feat: Authentication token generated

--- models/User.js
+++ models/User.js
@@ -4,14 +4,14 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = mongoose.Schema({
-  // fonction schema de mongoose //
+  // méthode schema de mongoose //
   email: { type: String, required: true, unique: true }, // les infos que l'on va stocker
   // unique permet d'éviter que plusieurs User s'enregistre avec la meme adresse mail // installer en parallèle npm install --save mongoose-unique-validator
   //cette technique unique:true  fonctionne mais ne suffit pas d'ou la nécessité du packagage pour éviter certaines erreurs illisibles
   password: { type: String, required: true }, // le hash sera de type string également
 });
 
-// ce validator, on l'applique au schéma avant d'en faire un modèle (on ne pourra pas avoir plusieurs utilisateur avec la même addresse mail)
+// ce validator, on l'applique au schéma avant d'en faire un modèle (on ne pourra pas avoir plusieurs utilisateurs avec la même adresse mail)
 userSchema.plugin(uniqueValidator); // méthode plugin avec comme argument à appliquer à cette méthode uniqueValidator
 
 // on va exporter ce schema sous forme de modèle
